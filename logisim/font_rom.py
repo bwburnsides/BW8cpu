06b88a8d63ec2a0fcfdf56fd923cf9afab6c68cb
brady <brady@burnsides.us>
2021-11-18 08:46:15 -0600

    removed some schematic and all logisim files

--- logisim/font_rom.py
+++
@@ -1,9 +0,0 @@
-with open("font_rom.bin", "w") as rom_file:
-    rom_file.write("v2.0 raw\n")
-
-    T = ["7e", "18", "18", "18", "18", "18", "18", "00"]
-    E = ["7e", "60", "60", "78", "60", "60", "7e", "00"]
-    S = ["3c", "66", "60", "3c", "06", "66", "3c", "00"]
-
-    rom = T + E + S
-    rom_file.write("\n".join(rom))
--- logisim/frame_buffer.py
+++
@@ -1,6 +0,0 @@
-with open("frame_rom.bin", "w") as rom_file:
-    rom_file.write("v2.0 raw\n")
-    rom_file.write("00\n01\n02\n00")
-
-    for _ in range((2 ** 13) - 4):
-        rom_file.write("\nff")
